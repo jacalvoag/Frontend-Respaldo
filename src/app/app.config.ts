690859c99d664cd7b72756fae89da77bd494b257
Jose Calvo <jacalvoaguilar@gmail.com>
2025-11-30 23:55:27 -0600

    Implementacion de imagenes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
